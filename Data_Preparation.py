8187d11d5932b10df138d27baf77fb81c82037df
dinhthixuanbinh <dinhthixuanbinh.work@gmail.com>
2024-04-08 14:36:34 +0700

    Data Preparation

---
+++ Data_Preparation.py
@@ -0,0 +1,9 @@
+'''
+chúng ta sẽ triển khai mô hình VAE-base Image Colorization dựa trên bài báo Learning
+Diverse Image Colorization để học cách tạo ra tập ảnh màu đa dạng về mặt kết quả. Cụ thể, ta sẽ xây
+dựng chương trình dựa trên bộ dữ liệu LFW (Labeled Faces in the Wild Home), một trong những bộ
+dữ liệu quan trọng và phổ biến trong lĩnh vực nhận dạng khuôn mặt. Bộ dữ liệu này chứa các hình ảnh
+của khuôn mặt được thu thập từ các bức ảnh chụp thực tế, bao gồm nhiều điều kiện ánh sáng, góc chụp
+và nền khác nhau.
+
+'''
